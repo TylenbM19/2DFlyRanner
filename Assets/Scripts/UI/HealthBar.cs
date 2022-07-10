9ac8a11deaf6858c9e575982714e6578ae996c8e
Igor <e-e-dsf-dsf@yandex.ru>
2022-07-10 15:59:13 +0300

    refact

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _image;
+
+    private void Start()
+    {
+        
+    }
+
+}
